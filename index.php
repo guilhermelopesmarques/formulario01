135f31e1a12a56e98176cd2325b93c0a935b9704
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2023-06-01 20:16:01 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>formulario01</title>
+</head>
+<body>
+    <form action="ola.php" method="POST">
+        <input type="text" name="nome">
+        <button type="submit" name="verificar">Verificar</button>
+    </form>
+</body>
+</html>
